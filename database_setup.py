7b2c02a3f2f137d3a673264a1a92bd75eb09ea00
Sid Ali MAHMOUDI <fs_mahmoudi@esi.dz>
2019-05-17 04:41:04 +0100

    Creating the database and setting it up

---
+++ database_setup.py
@@ -0,0 +1,65 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+"""Users class <==> users table"""
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    picture = Column(String(255))
+    
+    
+
+"""Programs class <==> programs table"""
+
+class Programs(Base):
+    __tablename__ = 'programs'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String(255), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    users = relationship(Users)
+
+    @property
+    def serialize(self):
+        # Return object data in serializeable format
+        return {
+            'id': self.id,
+            'title': self.name,
+        }
+
+
+"""Courses class <==> courses table"""
+
+class Courses(Base):
+    __tablename__ = 'courses'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String(255), nullable=False)
+    description = Column(String(255))
+    program_id = Column(Integer, ForeignKey('programs.id'), nullable=False)
+    programs = relationship(Programs)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    users = relationship(Users)
+
+    @property
+    def serialize(self):
+        # Return object data in serializeable format"""
+        return {
+            'id': self.id,
+            'program_id': self.program_id,
+            'title': self.title,
+            'description': self.description,
+        }
+
+engine = create_engine('sqlite:///books_catalog.db')
+Base.metadata.create_all(engine)
