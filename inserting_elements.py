2bec610a0238dc773cd071d6eab28748e91b6d38
SidaliMahmoudi <33000957+SidaliMahmoudi@users.noreply.github.com>
2019-05-22 10:36:33 +0100

    Update inserting_elements.py

--- inserting_elements.py
+++ inserting_elements.py
@@ -11,8 +11,8 @@ engine = create_engine('sqlite:///udacity_courses.db')
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
-# Linking the metadata of the Base class to our engine 
-# so that the declaratives can be accessed 
+# Linking the metadata of the Base class to our engine
+# so that the declaratives can be accessed
 # using a DBSession instance
 Base.metadata.bind = engine
 
@@ -60,11 +60,12 @@ session.commit()
 # The user_id is by default 1
 
 course = Courses(title='Machine Learning Engineer',
-                 description='''Learn advanced machine learning algorithms 
+                 description='''Learn advanced machine learning algorithms
                  and how to deploy your models to a production environment.
                  Gain practical experience using Amazon SageMaker to deploy
-                 trained models to a web application and evaluate its performance.
-                 AB test models and learn how to update the models as you gather more data.''',
+                 trained models to a web application and evaluate its
+                 performance. AB test models and learn how to update the
+                 models as you gather more data.''',
                  programs=program1,
                  )
 session.add(course)
@@ -73,8 +74,9 @@ session.commit()
 course = Courses(title='Deep Learning',
                  description='''Deep learning is driving advances in artificial
                  intelligence that are changing our world. Enroll now to build
-                 and apply your own deep neural networks to challenges like image
-                 classification and generation, time-series prediction, and model deployment.''',
+                 and apply your own deep neural networks to challenges like
+                 image classification and generation, time-series prediction,
+                 and model deployment.''',
                  programs=program1,
                  )
 session.add(course)
@@ -82,9 +84,9 @@ session.commit()
 
 course = Courses(title='Natural Language Processing',
                  description='''Master the skills to get computers to understand,
-                 process, and manipulate human language. Build models on real data,
-                 and get hands-on experience with sentiment analysis,
-                 machine translation, and more.''',
+                 process, and manipulate human language. Build models
+                 on real data, and get hands-on experience with
+                 sentiment analysis, machine translation, and more.''',
                  programs=program1,
                  )
 session.add(course)
@@ -101,8 +103,9 @@ session.commit()
 
 course = Courses(title='Data Engineer',
                  description='''Data Engineering is the foundation for the new world of
-                 Big Data. Enroll now to build production-ready data infrastructure,
-                 an essential skill for advancing your data career.''',
+                 Big Data. Enroll now to build production-ready data
+                 infrastructure, an essential skill for advancing your
+                 data career.''',
                  programs=program2,
                  )
 session.add(course)
@@ -110,7 +113,8 @@ session.commit()
 
 course = Courses(title='Predictive Analytics for Business',
                  description='''Learn to apply predictive analytics and
-                 business intelligence to solve real-world business problems''',
+                 business intelligence to solve real-world business
+                 problems''',
                  programs=program2,
                  )
 session.add(course)
@@ -120,7 +124,8 @@ course = Courses(title='Cloud Developer',
                  description='''Cloud development is the foundation for the new world
                  of software development. Enroll now to build and deploy
                  production-ready full stack apps at scale on AWS,
-                 an essential skill for advancing your web development career.''',
+                 an essential skill for advancing your web
+                 development career.''',
                  programs=program3,
                  )
 session.add(course)
@@ -128,9 +133,10 @@ session.commit()
 
 course = Courses(title='Cloud Dev Ops Engineer',
                  description='''Companies are looking for talented DevOps engineersto remain
-                 competitive in this agile world. Enroll now to operationalize infrastructure
-                 at scale and deliver applications and services at high velocity,
-                 an essential skill for advancing your career.''',
+                 competitive in this agile world. Enroll now to operationalize
+                 infrastructure at scale and deliver applications and services
+                 at high velocity, an essential skill for advancing
+                 your career.''',
                  programs=program3,
                  )
 session.add(course)
@@ -138,8 +144,9 @@ session.commit()
 
 course = Courses(title='C++ Developer',
                  description='''Learn C++, a high-performance programming language used in
-                 the world's most exciting engineering jobs -- from self-driving cars and
-                 robotics, to web browsers, media platforms, servers, and even video games.''',
+                 the world's most exciting engineering jobs -- from
+                 self-driving cars and robotics, to web browsers,
+                 media platforms, servers, and even video games.''',
                  programs=program4,
                  )
 session.add(course)
@@ -147,8 +154,9 @@ session.commit()
 
 course = Courses(title='Full Stack Developer',
                  description='''In this program, you’ll prepare for a job as a Full Stack
-                 Web Developer, and learn to create complex server-side web applications
-                 that use powerful relational databases to persistently store data.''',
+                 Web Developer, and learn to create complex server-side
+                 web applications that use powerful relational databases
+                 to persistently store data.''',
                  programs=program4,
                  )
 session.add(course)
@@ -156,9 +164,10 @@ session.commit()
 
 course = Courses(title='Blockchain Developer',
                  description='''Demand for blockchain developers is skyrocketing.
-                 In this program, you'll work with the Bitcoin and Ethereum protocols,
-                 build projects for real-world application, and gain the essential
-                 skills for a career in this dynamic space.''',
+                 In this program, you'll work with the Bitcoin and Ethereum
+                 protocols, build projects for real-world application,
+                 and gain the essential skills for a career in this
+                 dynamic space.''',
                  programs=program4,
                  )
 session.add(course)
@@ -166,8 +175,8 @@ session.commit()
 
 course = Courses(title='Robotics Software Engineer',
                  description='''Build hands-on projects to acquire core robotics
-                 software engineering skills: ROS, Gazebo, Localization, Mapping,
-                 SLAM, Navigation, and Path Planning.''',
+                 software engineering skills: ROS, Gazebo, Localization,
+                 Mapping, SLAM, Navigation, and Path Planning.''',
                  programs=program5,
                  )
 session.add(course)
@@ -175,9 +184,10 @@ session.commit()
 
 course = Courses(title='Self Driving Car Engineer',
                  description='''Self-driving cars are transformational technology,
-                 on the cutting-edge of robotics, machine learning, and engineering.
-                 Learn the skills and techniques used by self-driving car teams at
-                 the most advanced technology companies in the world.''',
+                 on the cutting-edge of robotics, machine learning,
+                 and engineering. Learn the skills and techniques used
+                 by self-driving car teams at the most advanced technology
+                 companies in the world.''',
                  programs=program5,
                  )
 session.add(course)
@@ -185,8 +195,9 @@ session.commit()
 
 course = Courses(title='Business Analytics',
                  description='''Gain foundational data skills applicable to any industry.
-                 Collect and analyze data, model business scenarios, and communicate your
-                 findings with SQL, Excel, and Tableau.''',
+                 Collect and analyze data, model business scenarios,
+                 and communicate your findings with SQL, Excel,
+                 and Tableau.''',
                  programs=program6,
                  )
 session.add(course)
@@ -194,14 +205,15 @@ session.commit()
 
 course = Courses(title='Predictive Analytics for Business',
                  description='''Learn to apply predictive analytics and
-                 business intelligence to solve real-world business problems''',
+                 business intelligence to solve real-world
+                 business problems''',
                  programs=program6,
                  )
 session.add(course)
 session.commit()
 
 course = Courses(title='Digital Marketing',
-                 description='''Gain real-world experience running live campaigns as you 
+                 description='''Gain real-world experience running live campaigns as you
                  learn from top experts in the field. Launch your career with a
                  360-degree understanding of digital marketing.''',
                  programs=program6,
